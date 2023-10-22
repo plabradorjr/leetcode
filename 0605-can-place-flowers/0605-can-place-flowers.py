0a223437259a17293b55a0a6264e4081fb786c52
Pancho Labrador Jr <plabradorjr@gmail.com>
2023-10-22 18:45:08 -0500

    Time: 148 ms (65.91%), Space: 16.6 MB (94.69%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,19 +1,20 @@
 class Solution:
     import math
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        count, prev = 0, 0
+        ableToPlantCounter = 0
+        prev = 0
 
-        for cur in flowerbed:
-            if cur == 1:
-                if prev == 1: # violation!
-                    count -= 1
+        for hole in flowerbed:
+            if hole == 1:
+                if prev == 1: # can't plant on the previous!
+                    ableToPlantCounter -= 1
                 prev = 1
-            else:
-                if prev == 1: # can't plant
-                    prev = 0 
-                else:
-                    count += 1
-                    prev = 1 # the cur plot gets taken
-            
-        return count >= n
+            if hole == 0:
+                if prev == 1: # can't plant here
+                    prev = 0
+                else: 
+                    ableToPlantCounter += 1
+                    prev = 1 # the hole gets taken
+
+        return ableToPlantCounter >= n
 
